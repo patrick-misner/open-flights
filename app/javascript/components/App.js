f710b6d6ae5b7e2449af22865a174bd9f6798850
Patrick Misner <misner.patrick@gmail.com>
2022-11-23 15:32:49 -0700

    route struggles

---
+++ app/javascript/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Airlines from './Airlines/Airlines'
+import Airline from './Airline/Airline'
+
+const App = () => {
+  return (
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Airlines />} />
+        <Route path="/airlines/:slug" element={<Airline />} />
+      </Routes>
+    </BrowserRouter>
+
+  )
+}
+
+export default App
