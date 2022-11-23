388f17e1db8cfd62a6ea86923ddb2fe0d8441bf3
Patrick Misner <misner.patrick@gmail.com>
2022-11-23 16:54:55 -0700

    spaceship

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -2,17 +2,20 @@
 // like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
 // of the page.
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-import App from '../components/App'
-import { BrowserRouter, BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import App from '../components/App.js';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Router>
-      <Route path="*" element={<App />} />
-    </Router>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>,
+    </BrowserRouter>,
+
     document.body.appendChild(document.createElement('div')),
   )
 })
