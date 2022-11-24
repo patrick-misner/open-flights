cb8c46ed1afa2f170818e190e9466d608c5f75a1
Patrick Misner <misner.patrick@gmail.com>
2022-11-23 18:37:59 -0700

    woohoo

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,18 +1,14 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Airlines from './Airlines/Airlines'
-import Airline from './Airline/Airline'
+import { Route, Routes } from 'react-router-dom';
+import Airlines from './Airlines/Airlines';
+import Airline from './Airline/Airline';
 
 const App = () => {
   return (
-
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Airlines />} />
-        <Route path="/airlines/:slug" element={<Airline />} />
-      </Routes>
-    </BrowserRouter>
-
+    <Routes>
+      <Route path="/" element={<Airlines />} />
+      <Route path="/airlines/:slug" element={<Airline />} />
+    </Routes>
   )
 }
 
