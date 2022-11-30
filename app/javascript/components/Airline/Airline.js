2fd820df0d40dcd91a537fa132cf7d67a5ca2eb9
Patrick Misner <misner.patrick@gmail.com>
2022-11-30 03:58:49 -0700

    airlines working, airline almost ready

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from "axios"
 
 const Airline = () => {
-  return <div>This is the Airlines#show view for our app</div>
+  const [airline, setAirline] = useState({})
+  const [review, setReview] = useState({})
+  const { slug } = useParams()
+
+  useEffect(() => {
+
+    const url = `/api/v1/airlines/${slug}`
+    // console.log(slug)
+
+    axios.get(url)
+      .then(resp => setAirline(resp.data))
+      .catch(resp => console.log(resp))
+  }, [])
+  return (
+    <div className="wrapper">
+      <div className="column">
+        <div className=""
+      </div>
+      <div className="column"></div>
+
+    </div>
+  )
 }
 
 export default Airline
