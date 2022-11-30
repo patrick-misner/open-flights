b82a2f9c836789d26971f298dfb44ba32c85274c
Patrick Misner <misner.patrick@gmail.com>
2022-11-30 03:58:55 -0700

    commit

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,27 +1,60 @@
 import React, { useState, useEffect, Fragment } from 'react'
 import axios from "axios"
+import Airline from './Airline'
+import styled from 'styled-components'
+
+const Home = styled.div`
+  text-align: center;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+`
+const Header = styled.div`
+  padding: 100px 100px 10px 100px;
+
+  h1 {
+    font-size: 42px;
+  }
+`
+const Subheader = styled.div`
+  font-weight: 300;
+  font-size: 26px;
+`
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  width: 100%;
+  padding: 20px;
+`
+
 
 const Airlines = () => {
   const [airlines, setAirlines] = useState([])
   useEffect(() => {
-    // get airlines from API
-    // update airlines in our state
     axios.get('/api/v1/airlines.json')
-      .then(resp => {
-        setAirlines(resp.data.data)
-      })
+      .then(resp => setAirlines(resp.data.data))
       .catch(resp => console.log(resp))
   }, [airlines.length])
 
-  const list = airlines.map(item => {
-    return (<li key={item.attributes.name}>{item.attributes.name}</li>)
+  const grid = airlines.map(item => {
+    return (
+      <Airline
+        key={item.attributes.name}
+        attributes={item.attributes}
+      />
+    )
   })
   return (
-    <Fragment>
-
-      <div>This is the Airlines#index view for our app</div>
-      <ul>{list}</ul>
-    </Fragment>
+    <Home>
+      <Header>
+        <h1>OpenFlights</h1>
+        <Subheader>Honest, unbiased airline reviews</Subheader>
+      </Header>
+      <Grid>
+        {grid}
+      </Grid>
+    </Home>
   )
 }
 
