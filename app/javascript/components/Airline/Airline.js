a903475f237ee08022d8b1b30e2a7ec63ded6d4d
Patrick Misner <misner.patrick@gmail.com>
2022-12-01 06:24:36 -0700

    reviews done

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,10 +1,35 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Fragment } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from "axios"
+import Header from './Header'
+import styled from 'styled-components'
+import ReviewForm from "./ReviewForm"
+import Review from './Review'
+
+
+const Wrapper = styled.div`
+margin-left: auto;
+margin-right: auto;
+display: grid;
+grid-template-columns: repeat(2, 1fr);
+`
+const Column = styled.div`
+background: #fff;
+height: 100vh;
+overflow: scroll;
+
+&:last-child {
+  background: #000
+}
+`
+const Main = styled.div`
+  padding-left: 50px;
+`
 
 const Airline = () => {
   const [airline, setAirline] = useState({})
   const [review, setReview] = useState({})
+  const [loaded, setLoaded] = useState(false)
   const { slug } = useParams()
 
   useEffect(() => {
@@ -13,17 +38,83 @@ const Airline = () => {
     // console.log(slug)
 
     axios.get(url)
-      .then(resp => setAirline(resp.data))
+      .then(resp => {
+        setAirline(resp.data)
+        setLoaded(true)
+      })
       .catch(resp => console.log(resp))
   }, [])
-  return (
-    <div className="wrapper">
-      <div className="column">
-        <div className=""
-      </div>
-      <div className="column"></div>
 
-    </div>
+  const handleChange = (e) => {
+    e.preventDefault()
+
+    setReview(Object.assign({}, review, { [e.target.name]: e.target.value }))
+
+    console.log('review:', review)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const csrfToken = document.querySelector('[name=csrf-token]').content
+    axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
+
+    const airline_id = airline.data.id
+    axios.post('/api/v1/reviews', { review, airline_id })
+      .then(resp => {
+        const included = [...airline.included, resp.data.data]
+        setAirline({ ...airline, included })
+        setReview({ title: '', description: '', score: 0 })
+      })
+      .catch(resp => { })
+  }
+
+  const setRating = (score, e) => {
+    e.preventDefault()
+
+    setReview({ ...review, score })
+  }
+
+  let reviews
+  if (loaded && airline.included) {
+    reviews = airline.included.map((item, index) => {
+      return (
+        <Review
+          key={index}
+          attributes={item.attributes}
+        />
+      )
+    })
+  }
+
+  return (
+    <Wrapper>
+      {
+        loaded &&
+        <Fragment>
+          <Column>
+            <Main>
+
+              <Header
+                attributes={airline.data.attributes}
+                reviews={airline.included}
+              />
+
+              {reviews}
+            </Main>
+          </Column>
+          <Column>
+            <ReviewForm
+              handleChange={handleChange}
+              handleSubmit={handleSubmit}
+              setRating={setRating}
+              attributes={airline.data.attributes}
+              review={review}
+
+            />
+          </Column>
+        </Fragment>
+      }
+    </Wrapper>
   )
 }
 
